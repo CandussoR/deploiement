e1173c15b2b1a949dc22a06ebaa01956ac4dc28f
Romain Candusso <90688847+CandussoR@users.noreply.github.com>
2022-11-09 20:43:48 +0100

    Update install_server.sh

--- install_server.sh
+++ install_server.sh
@@ -10,19 +10,29 @@
 	apt -y composer install
 	apt install -y php8.1-fpm php-pgsql openssl php-common php-curl php-json php-mbstring php-mysql php-xml php-zip
 	apt install -y ufw nginx
-	ufw allow 'Nginx HTTP'
-	ufw allow 'OpenSSH'
-	ufw enable
+	
+	# pare-feu commenté car cause des problèmes avec le HTTPS plus tard
+	#ufw allow 'Nginx HTTP'
+	#ufw allow 'OpenSSH'
+	#ufw enable
 
-	scp /home/romain/Code/deploiement/default root@$1:/etc/nginx/sites-available/
-	nginx -t
-	systemctl restart nginx
-
+	# clone le repo Laravel dans Nginx pour pouvoir s'y référer dans le fichier de config serveur
 	cd /var/www/html
 	git clone https://github.com/laravel/laravel.git
+	# Copie le fichier de config server pour l'hotel-arth dans nginx
+	scp /home/romain/Code/deploiement/hotel-arth root@$1:/etc/nginx/sites-available/
+	# Crée un lien symbolique de sites available vers sites enabled pour que ça puisse marcher
+	ln -s /etc/nginx/sites-available/hotel-arth /etc/nginx/sites-enabled/hotel-arth
+	# test nginx
+	nginx -t
+	# le restart pour mettre à jour la config
+	systemctl restart nginx
+
+	# on installe tranquillement Laravel
 	cd laravel
 	git checkout 8.x
 	composer -n install
+	# copier-coller du fichier .env modifé pour permettre la commande php artisan serve
 	scp /home/romain/Code/deploiement/.env root@$1:/var/www/html/laravel/
 	
 #else
